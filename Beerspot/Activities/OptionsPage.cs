067553b1dbfd0efb0a2c0e78a1e5d819587e3671
mati792 <mati792@onet.pl>
2017-04-20 21:45:34 +0200

    Dodanie menu i nagłówka

---
+++ Beerspot/Activities/OptionsPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+
+namespace Beerspot.Activities
+{
+    [Activity(Label = "", Theme = "@style/Theme.NoTitle")]
+    public class OptionsPage : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.OptionsPage);
+            // Create your application here
+            Button mapaButton = FindViewById<Button>(Resource.Id.mapa_button);
+            mapaButton.Click += GoToMap_Click;
+        }
+
+        private void GoToMap_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+
+    }
+}
--- Beerspot/MainActivity.cs
+++ Beerspot/MainActivity.cs
@@ -5,20 +5,30 @@ using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
 using Android.Locations;
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Views;
 
 namespace Beerspot
 {
-    [Activity(Label = "Beerspot", Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "", Icon = "@drawable/icon",  Theme = "@style/MyTheme")]
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
 
-            Button callMapButton = FindViewById<Button>(Resource.Id.callMapButton);
+   
+
+        //SupportActionBar.Title = "beerspot";
+
+        //var mTitle = (TextView)toolbar.FindViewById(Resource.Id.toolbar_title);
+
+        Button callMapButton = FindViewById<Button>(Resource.Id.callMapButton);
 
             callMapButton.Click += (sender, e) =>
             {
@@ -27,5 +37,30 @@ namespace Beerspot
                 StartActivity(mapIntent);
             };
         }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.MenuButton, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {/*
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                ToastLength.Short).Show();*/
+
+            if (item.ItemId == Resource.Id.menu_button)
+            {
+                StartActivity(typeof(Activities.OptionsPage));
+                return true;
+            }
+            else
+                return base.OnOptionsItemSelected(item);
+            
+
+        }
+
     }
+
+
 }
